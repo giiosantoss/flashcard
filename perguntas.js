ba253b26eadaa7bea8232d07fd0b1efc6bb97259
giiosantoss <141050562+giiosantoss@users.noreply.github.com>
2024-09-13 09:55:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Português',
+    'De onde é a invenção do chuveiro?',
+    'Brasil'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da neblina'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o maior animal terrestre?',
+    'Elefante africano'
+)
+
+criaCartao(
+    'Química',
+    'Qual metal cujo símbolo químico é o "Au"?',
+    'Ouro'
+)
+
+criaCartao(
+    'Hístoria',
+    'Em que século o continente europeu foi devastado pela peste bubônica?',
+    'No século XIV.'
+)
